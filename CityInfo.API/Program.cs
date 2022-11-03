f1580695a7455cdc70ab75208032fd30c15c207c
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-11-03 20:53:44 +0530

    setting up

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,9 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,11 +13,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+//app.UseHttpsRedirection();
 
-app.UseHttpsRedirection();
+//app.UseAuthorization();
 
-app.UseAuthorization();
+//app.MapControllers();
 
-app.MapControllers();
+app.Run(async (context) =>
+{
+    await context.Response.WriteAsync("hello world");
+});
 
 app.Run();
