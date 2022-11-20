8eedc780c23f278c708db39f1f3afa57eb2ff295
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-11-21 01:09:27 +0530

    seed the database

--- CityInfo.API/Migrations/20221120193557_01_CityInfoDb_SeedingData.Designer.cs
+++ CityInfo.API/Migrations/20221120193557_01_CityInfoDb_SeedingData.Designer.cs
@@ -2,6 +2,7 @@
 using CityInfo.API.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CityInfo.API.Migrations
 {
     [DbContext(typeof(CityInfoContext))]
-    partial class CityInfoContextModelSnapshot : ModelSnapshot
+    [Migration("20221120193557_01_CityInfoDb_SeedingData")]
+    partial class _01_CityInfoDb_SeedingData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.11");
---
+++ CityInfo.API/Migrations/20221120193557_01_CityInfoDb_SeedingData.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CityInfo.API.Migrations
+{
+    public partial class _01_CityInfoDb_SeedingData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "CityId", "Description", "Name" },
+                values: new object[] { 1, "The best city in usa", "New York City" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "CityId", "Description", "Name" },
+                values: new object[] { 2, "Capital of Indunisia", "Jakartha" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "CityId", "Description", "Name" },
+                values: new object[] { 3, "A Nice city of Japan", "Tokyo" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "PointOfInterestId", "CityId", "Description", "Name" },
+                values: new object[] { 1, 1, "There are so many trees", "Central park" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "PointOfInterestId", "CityId", "Description", "Name" },
+                values: new object[] { 2, 1, "There are so many people", "Central Market" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "PointOfInterestId", "CityId", "Description", "Name" },
+                values: new object[] { 3, 2, "There are so many buses", "Transport Center" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "PointOfInterestId", "CityId", "Description", "Name" },
+                values: new object[] { 4, 3, "There are so many sea food", "Fish Market" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "PointOfInterestId", "CityId", "Description", "Name" },
+                values: new object[] { 5, 2, "There are so many boats", "Harbor" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "PointOfInterestId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "PointOfInterestId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "PointOfInterestId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "PointOfInterestId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "PointOfInterestId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "CityId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "CityId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "CityId",
+                keyValue: 3);
+        }
+    }
+}
